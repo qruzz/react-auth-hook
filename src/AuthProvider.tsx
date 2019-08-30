dd4a76842eefef40c2d8775528e8a491fadbf486
qruzz <m9ssen@gmail.com>
2019-08-30 20:48:04 +0100

    feat: expose AuthProvider
    create the context instantiate the auth0 client

---
+++ src/AuthProvider.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect } from 'react';
+import Auth0 from 'auth0-js';
+import { useImmerReducer } from 'use-immer';
+import { authReducer, AuthState, AuthAction } from './authReducer';
+import { handleAuthResult } from './useAuth';
+
+export interface AuthContext {
+	state: AuthState;
+	dispatch: React.Dispatch<AuthAction>;
+	auth0Client: Auth0.WebAuth;
+	callbackDomain: string;
+	navigate: any;
+}
+
+export const AuthContext = React.createContext<AuthContext>({} as AuthContext);
+
+export interface AuthProvider {
+	auth0Domain: string;
+	auth0ClientId: string;
+	navigate: any;
+	children: React.ReactNode;
+}
+
+export function AuthProvider({
+	auth0Domain,
+	auth0ClientId,
+	navigate,
+	children,
+}: AuthProvider) {
+	const callbackDomain = window
+		? `${window.location.protocol}//${window.location.host}`
+		: 'http://localhost:3000';
+
+	const auth0Client = new Auth0.WebAuth({
+		domain: auth0Domain,
+		clientID: auth0ClientId,
+		redirectUri: `${callbackDomain}/auth_callback`,
+		audience: `https://${auth0Domain}/api/v2/`,
+		responseType: 'token id_token',
+		scope: 'openid profile email',
+	});
+
+	const [state, dispatch] = useImmerReducer<AuthState, AuthAction>(
+		authReducer,
+		{
+			user: null,
+			authResult: null,
+			expiresOn: null,
+		}
+	);
+
+	const [contextValue, setContextValue] = React.useState<AuthContext>({
+		state,
+		dispatch,
+		auth0Client,
+		callbackDomain,
+		navigate,
+	});
+
+	useEffect(() => {
+		setContextValue({ ...contextValue, state });
+	}, [state]);
+
+	useEffect(() => {
+		auth0Client.checkSession({}, (error, authResult) => {
+			if (error) {
+				dispatch({
+					type: 'AUTH_ERROR',
+					errorType: 'checkSession',
+					error,
+				});
+			} else {
+				handleAuthResult({ dispatch, auth0Client, authResult });
+			}
+		});
+	}, []);
+
+	return (
+		<AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+	);
+}
